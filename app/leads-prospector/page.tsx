67497a58ec1a27c12bd318ef16e625cc7429e10d
Melinda <SEU_EMAIL_DO_GITHUB>
2026-05-04 14:58:15 +0000

    fix: corrigir renderizacao da pagina leads prospector

--- app/leads-prospector/page.tsx
+++ app/leads-prospector/page.tsx
@@ -1,9 +1,11 @@
 import LeadProspectorAgentPanel from "@/components/leads-prospector/LeadProspectorAgentPanel";
 import LeadsProspectorCRM from "@/components/leads-prospector/LeadsProspectorCRM";
 
-export const dynamic = "force-dynamic";
-
 export default function LeadsProspectorPage() {
-  return <LeadProspectorAgentPanel />
-      <LeadsProspectorCRM />;
+  return (
+    <>
+      <LeadProspectorAgentPanel />
+      <LeadsProspectorCRM />
+    </>
+  );
 }
